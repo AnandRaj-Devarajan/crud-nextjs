c54a130b590092499c164bf7a07896bee6207638
Anand raj <anand@micropyramid.com>
2024-04-01 22:42:23 +0530

    json create,edit

--- src/app/constants.js
+++ src/app/constants.js
@@ -1,4 +1,4 @@
-export const data = {
+export const jsonData = {
     "payment": {
       "id": 1,
       "slug": "none",
--- src/app/json/page.js
+++ src/app/json/page.js
@@ -1,60 +1,115 @@
 'use client'
-import { Card, Divider, Flex, Select, Typography } from 'antd'
+import React, { useEffect, useState } from 'react'
+import { Button, Card, Col, Divider, Flex, Input, Row, Select, Space, Switch, Typography } from 'antd'
 import { Option } from 'antd/es/mentions'
-import React from 'react'
-import { data } from '../constants'
+import { jsonData } from '../constants'
+import { CloseOutlined, PlusOutlined, SettingOutlined } from '@ant-design/icons'
 
 
 const page = () => {
+  const [data, setData] = useState({})
+
+  useEffect(() => {
+    setData(jsonData)
+  }, [])
+
+  const addItem = () => {
+    const newItem = {
+      id: data.payment.deposit.length + 1,
+      custom_input: [
+        {
+          type: 'options',
+          label: 'card1',
+        },
+        {
+          type: 'options',
+          label: 'card1',
+        },
+      ],
+      country_sorting: [
+        {
+          sort: 1,
+          action: true,
+          country: 'India',
+        },
+        {
+          sort: 2,
+          action: false,
+          country: 'USA',
+        },
+      ],
+    };
+
+    setData({
+      ...data,
+      payment: {
+        ...data.payment,
+        deposit: [...data.payment.deposit, newItem],
+      },
+    });
+  };
+  const removeItem = (itemId) => {
+    const updatedDeposit = data.payment.deposit.filter(item => item.id !== itemId);
+
+    setData({
+      ...data,
+      payment: {
+        ...data.payment,
+        deposit: updatedDeposit,
+      },
+    });
+  };
 
   return (
     <>
-      {data.payment.deposit.map((item) => (
-        <Card key={item.id} title={item.type}>
-          {item.custom_input.map((input, index) => (
-            <div key={index} >
-              {input.type === 'options' && (
-                <>
-                  <Typography>{input.label}</Typography>
-                  <Divider orientation="left"></Divider>
-                  <Select defaultValue="" style={{ width: 200, marginBottom: 10 }}>
-                    {item.country_sorting.map((country) => (
-                      <Option key={country.country} value={country.country}>
-                        {country.country}
-                      </Option>
-                    ))}
-                  </Select>
-                </>
-              )}
-            </div>
-          ))}
-        </Card>
-      ))}
-      {data.payment.withdrawl.map((item) => (
-        <Card key={item.id} title={item.type}>
-          {item.custom_input.map((input, index) => (
-            <div key={index} >
-              {input.type === 'options' && (
-                <>
-                  <Typography>{input.label}</Typography>
-                  <Divider orientation="left"></Divider>
-                  <Select defaultValue="" style={{ width: 200, marginBottom: 10 }}>
-                    {item.country_sorting.map((country) => (
-                      <Option key={country.country} value={country.country}>
-                        {country.country}
-                      </Option>
-                    ))}
-                  </Select>
-                </>
-              )}
-            </div>
-          ))}
-        </Card>
-      ))}
+      <div style={{ marginTop: '5vh' }}>
+        <Card>
+          <div>
+            {
+              data?.payment?.deposit.map((item) => (
+                < Col span={14} key={item?.id}>
+                  <Typography.Title level={5}>Deposit</Typography.Title>
+                  <Card style={{ height: '100%', minHeight: '40vh' }}>
+                    <Flex horizontal style={{ margin: '-24px', height: '40vh', flexDirection: 'row', alignItems: 'flex-start', justifyContent: 'space-between', minWidth: '100%' }}>
+                      <Flex horizontal wrap='wrap' gap={'middle'} style={{ padding: 10 }} >
+                        {item?.custom_input.map((val) =>
+                          val?.type === 'options' &&
+                          <div>
+                            <Typography>{val.label}</Typography>
+                            <Select defaultValue={'card1'} style={{ width: 200 }}>
+                              <Option value={'card1'}>
+                                card1
+                              </Option>
+                            </Select>
+                          </div>
+                        )}
+                        {item?.country_sorting.map((val) =>
+                          val?.["sort"] &&
+                          <div>
+                            <Typography>{val?.country}</Typography>
+                            <Switch checked={val?.action} />
+                          </div>
+                        )}
+                      </Flex>
+                      <Divider type='vertical' style={{ height: 'auto' }} />
+                      <Flex vertical style={{ minHeight: '100%', justifyContent: 'space-between', alignItems: 'flex-end', padding: '4px', borderLeft: '0.5px solid #0000001f' }}>
+                        <Button shape="circle" style={{ border: 'none' }} icon={<CloseOutlined />} onClick={() => removeItem(item?.id)} />
+                        <Button shape="circle" style={{ border: 'none' }} icon={<SettingOutlined />} />
+                      </Flex>
+                    </Flex>
+                  </Card>
+                </Col>)
+              )
+            }
+          </div>
+          <Col span={12}>
+            <Button type="primary" style={{ marginTop: 10 }} icon={<PlusOutlined />} onClick={addItem}>
+              Add Item
+            </Button>
+          </Col>
+        </Card >
+      </div >
     </>
   )
 }
-
 export default page
-
-
